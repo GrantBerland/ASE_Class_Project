4a6469fd4c003e53a2a1fc7a5ca346e59319ccc5
GrantBerland <grant.berland.gb@gmail.com>
2020-05-07 17:03:35 -0600

    cleaned up code, removed duplicated functions

--- PF_code/myParticleFilter.m
+++ PF_code/myParticleFilter.m
@@ -1,12 +1,12 @@
 function [state_est, covar, measurements] = myParticleFilter(timeArray, x0, noiseModel, nParts, dt)
 
-% Set seed
-rng(100);
+state_est = zeros(length(x0), length(timeArray)); % 9 x tSpan
+covar     = zeros(length(x0), length(x0), length(timeArray)); % 9 x 9 x tSpan
 
-% Pre allocate data
+% Assign initial uniform weight to all particles
 weights = ones(nParts, 1)/nParts;
 
-resampleThreshold = 1;
+resampleThreshold = 0.6;
 
 initialStatePDF = x0;
 
@@ -16,59 +16,24 @@ initialStatePDF = x0;
 parts = datasample(pdf_x, nParts, 'Replacement', true);
 
 for i = 1:timeArray
-    % Do filtering
     
+    % Get posterior PDF 
+    [posterior, oldParticles, w] = sampleFromProposalDist(parts, x0, noiseModel, pdf_yk_xki);
     
-end
-
-end
-function [posterior, oldParticles, w] = sampleFromProposalDist(parts,x0,noiseModel,pdf_yk_xki)
-%(TODO: insert algo)
-
-for i = 1:nParts
+    % Get weights
+    weights = computWeights();
     
-%To sample for particles, first need to sample noise 
-%This depends on what noise model we are using 
-
-
-%To sample can propagate particles k-1 and noise through dynamics
-%C: I will leave this to you - need to save all particles - note we are
-%doing one particle at a time 
-
-%To find weights - use p(y_k|x_k^i) and evaluate at x_k^i
-
-w(i) = pdf_yk_xki;
-
-end
-
-
-for i = 1:timeArray
-    
-    %Need to know p(y_k|x_k^i) fxn so that we can plug in particle i
-    %generated
-    pdf_yk_xki; 
-    
-    [posteriorDist, oldParticles, weights] = sampleFromProposalDist(parts,x0,noiseModel,pdf_yk_xki);
-    
-    % Renormalize weights
+    % Normalize weights s.t. sum(weights) == 1
     weights = weights / sum(weights);
     
-    %C: I commented this part out because according to table 3.4, this is not
-    %done for bootstrap particle filter since it resamples every time 
-    %{
-    % Calculate effective # of particles (TODO: insert algo)
-    Neff = 0;
-    
-    if Neff < resampleThreshold
-        parts = bootStrapParticles(parts);
+    % Test if particles need to be resampled
+    Neff = 1/sum(weights.^2);
+    if Neff < resampleThreshold*nParts
+        [parts, weights] = resample(parts, weights);
     end
-    %}
     
-    [parts, weights] = bootStrapParticles(parts, weights, 'myAlgo');
-    
-    % MMSE or MAP state estimate
-    state_est = getEstimate(parts, linspace(min(parts), max(parts), nParts*100), 'MAP');
-    
+    % Get estimate at current timestep
+    [state_est(:,i), covar(:,:,i)] = getEstimate(parts, weights, support, 'MAP');
 end
 
 end
@@ -91,73 +56,28 @@ w(i) = pdf_yk_xki;
 end
 
 end
-function [newParticles, newWeights] = bootStrapParticles(oldParticles, oldWeights, resamplingType)
-
-switch resamplingType
-    
-case 'myAlgo'
-        
-    %Allocate space for resampled parameters
-    newParticles = zeros(length(oldParticles), 1);
-
-    % Sort weights of particles in descending order (first = max)
-    [weight_cdf, oldWeightIndices] = sort(oldWeights, 'descend');
-
-    for samples = 1:length(oldParticles)
-
-        % Draw radom sample of 0 to 1 from uniform distribution
-        u = rand;
-
-        % Find first index where cdf is greater than Unif[0,1]
-        part_index = find( weight_cdf > u , 1);
-
-        %Add this to new particle list 
-        newParticles(samples) = oldParticles(part_index);
-
-    end
-
-    % Uniform new weights
-    newWeights = ones(length(oldWeight), 1) / length(oldWeights);
-
-case 'stock'
-
-    [newParticles, newWeights] = resample(oldParticles, oldWeights);
-
-otherwise 
-    
-    error("Enter a valid resampling type to function: bootStrapParticles")
-    
-end
-
-end
-function [estimate, covar] = getEstimate(particles, support, typeOfEstimator)
+function [estimate, covar] = getEstimate(particles, weights, support, typeOfEstimator)
 
 switch typeOfEstimator
-    
     case 'MMSE'
-    
-    %(TODO: insert algo)
-    
-    estimate = 1;
-    covar    = 1;
-    
-    case 'MAP'
-    
-    % Kernel density estimate to generate a PDF
-    density = ksdensity(particles, support, 'kernel', 'epanechnikov');
-    
-    % Locate mode of posterior
-    [~, idx] = max(density);
+        %(TODO: insert algo)
 
-    % Estimate state and covariance within the 
-    % resolution of the passed-in support
-    estimate = support(idx);
-    covar    = var(density);
-    
+        estimate = 1;
+        covar    = 1;
+    case 'MAP'  
+        % Kernel density estimate to generate a PDF
+        density = ksdensity(particles, support, ...
+                            'kernel', 'epanechnikov');
+
+        % Locate mode of posterior
+        [~, idx] = max(density);
+
+        % Estimate state and covariance within the 
+        % resolution of the passed-in support
+        estimate = support(idx);
+        covar    = var(density);
     otherwise
-        
-    error("Enter a valid estimator type to function: getEstimate()")
-        
+        error("Enter a valid estimator type to function: getEstimate()")
 end
 
 end
